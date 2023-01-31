3de5ea64d488f6b90f695826cdabb51cf56564b3
afazad <abulfajle405@gmail.com>
2023-01-31 23:26:47 +0530

    Initial Commit

---
+++ LinearSearch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class LinearSearch{
+    //linear search also known as Sequential search
+    public static void main(String[] args){
+        int arr[] = {52, 63, 74, 85, 45, 59, 99};
+        int item = 59;
+        int temp = 0; // if item is not found 
+        System.out.print("Item found at index postion : ");
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == item){
+                System.out.println(i);
+                temp= temp+1;
+            }
+        }
+        if(temp==0){
+            System.out.println("item not found at any index");
+        }
+    }
+}
